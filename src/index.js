add77c7a40facb1d45360ee837bef0b14df2bca4
Dharmesh <dharmesh.hemaram@gmail.com>
2020-10-06 10:52:17 +0530

    Added GA service for error part

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.scss'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 import { disableContextMenu } from './app/util/helper'
+import { GAService } from '@dhruv-techapps/core-common'
 
 window.react_env = process.env
 
@@ -12,6 +13,15 @@ ReactDOM.render(<App />, document.getElementById('root'))
 if (process.env.NODE_ENV !== 'development') {
   disableContextMenu()
 }
+
+console.error = (function () {
+  const { error } = console
+  return function (...args) {
+    GAService.error({ name: 'OptionsPageError', stack: args[0] })
+    error.apply(console, args)
+  }
+}())
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
