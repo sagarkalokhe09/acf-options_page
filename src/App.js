2f0d08e35080304969f2c055249b4a571fcc784c
Dharmesh <dharmesh.hemaram@gmail.com>
2020-12-26 14:32:10 +0530

    added amazon ads

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Configs from './app/main/configs'
 import ToastHandler from './app/components/ToastHandler'
 import { Container } from 'react-bootstrap'
 import { ManifestService } from '@dhruv-techapps/core-common'
-import GoogleAds from './app/components/GoogleAds'
 import ExtensionNotFoundModel from './app/extension-not-found.modal'
 
 function App () {
@@ -30,7 +29,7 @@ function App () {
     <Footer version={manifest.version || ''}/>
     <ToastHandler ref={toastRef} />
     <ExtensionNotFoundModel ref={extensionNotFoundRef} />
-    <GoogleAds client={process.env.REACT_APP_GOOGLE_ADS_CLIENT} slot={process.env.REACT_APP_GOOGLE_ADS_SLOT} format="auto"/>
+    {/* <GoogleAds client={process.env.REACT_APP_GOOGLE_ADS_CLIENT} slot={process.env.REACT_APP_GOOGLE_ADS_SLOT} format="auto"/> */}
   </Container>
 }
 
