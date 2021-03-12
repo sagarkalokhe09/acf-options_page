a2a81733d77a854f72fdcf60bfd314037f41df66
Dharmesh <dharmesh.hemaram@gmail.com>
2021-03-12 15:36:49 +0530

    Fixed addon condition check

--- src/index.js
+++ src/index.js
@@ -30,8 +30,9 @@ console.error = (function () {
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register()
+serviceWorker.unregister()
 
+/*
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('./firebase-messaging-sw.js')
@@ -39,3 +40,4 @@ if ('serviceWorker' in navigator) {
       GTAG.exception({ description: message, fatal: true })
     })
 }
+*/
