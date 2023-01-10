6ebe58099a67415dec278d514da86962297f927a
Dhruv TechApps <75973635+Dhruv-Tech-Apps@users.noreply.github.com>
2023-01-10 13:04:07 +0530

    Update authConfig.js
    Signed-off-by: Dhruv TechApps <75973635+Dhruv-Tech-Apps@users.noreply.github.com>

--- src/authConfig.js
+++ src/authConfig.js
@@ -14,7 +14,7 @@ import { b2cPolicies } from './policies'
  */
 export const msalConfig = {
   auth: {
-    clientId: process.env.CLIENT_ID,
+    clientId: process.env.AZURE_APP_CLIENT_ID,
     authority: b2cPolicies.authorities.signUpSignIn.authority, // Choose sign-up/sign-in user-flow as your default.
     knownAuthorities: [b2cPolicies.authorityDomain] // You must identify your tenant's domain as a known authority.
   },
