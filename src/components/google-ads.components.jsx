93a4137b05da8f5088639cd50ab0f7cf730d0453
Dhruv TechApps <75973635+Dhruv-Tech-Apps@users.noreply.github.com>
2023-02-08 10:46:31 +0530

    Update google-ads.components.jsx
    Signed-off-by: Dhruv TechApps <75973635+Dhruv-Tech-Apps@users.noreply.github.com>

--- src/components/google-ads.components.jsx
+++ src/components/google-ads.components.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { Col, Row } from 'react-bootstrap'
 
-export function GoogleAds({ client, slot, format, className, setLoaded }) {
+export function GoogleAds({ client, slot, className, setLoaded }) {
   useEffect(() => {
     // eslint-disable-next-line no-extra-semi
     ;(window.adsbygoogle = window.adsbygoogle || []).push({})
@@ -16,7 +16,7 @@ export function GoogleAds({ client, slot, format, className, setLoaded }) {
   return (
     <Row>
       <Col xs={12} className='text-center'>
-        <ins className={`${className} adsbygoogle`} style={{ display: 'block' }} data-ad-client={client} data-ad-slot={slot} data-ad-format={format} data-full-width-responsive='true' />
+        <ins className={`${className} adsbygoogle`} style={{ display: 'inline-block', width: '728px', height: '90px' }} data-ad-client={client} data-ad-slot={slot} />
       </Col>
     </Row>
   )
@@ -24,13 +24,11 @@ export function GoogleAds({ client, slot, format, className, setLoaded }) {
 GoogleAds.defaultProps = {
   className: 'mb-3',
   client: process.env.REACT_APP_GOOGLE_ADS_CLIENT,
-  slot: process.env.REACT_APP_GOOGLE_ADS_SLOT,
-  format: 'auto'
+  slot: process.env.REACT_APP_GOOGLE_ADS_SLOT
 }
 GoogleAds.propTypes = {
   client: PropTypes.string,
   slot: PropTypes.string,
-  format: PropTypes.string,
   className: PropTypes.string,
   setLoaded: PropTypes.func.isRequired
 }
